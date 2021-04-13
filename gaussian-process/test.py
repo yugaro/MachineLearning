1696328d600caaba5503b86b2b9cc7b390f353d9
yugaro <yugaro.yugaro@gmail.com>
2021-04-13 11:01:01 +0900

    Initial Commit

--- gaussian-process/test.py
+++ gaussian-process/test.py
@@ -3,6 +3,8 @@ import torch
 import pyro
 import pyro.contrib.gp as gp
 import pyro.distributions as dist
+# import numpy as np
+import math
 pyro.set_rng_seed(100)
 
 N = 20
@@ -12,13 +14,42 @@ plt.plot(X.numpy(), y.numpy(), 'kx')
 plt.savefig('./image/fig01.pdf')
 plt.close()
 
-variance = torch.tensor(0.1)
-lengthscale = torch.tensor(0.1)
+variance = torch.tensor(1)
+lengthscale = torch.tensor([0.1, 0.2, 0.3, 0.5])
 noise = torch.tensor(0.01)
 
-kernel = gp.kernels.RBF(input_dim=1, variance=variance,
+kernel = gp.kernels.RBF(input_dim=4, variance=variance,
                         lengthscale=lengthscale)
+x1 = torch.tensor([[1, 1, 1, 1]])
+x2 = torch.tensor([[1, 1, 1, 1]])
+x3 = torch.tensor([[2, 2, 2, 2]])
+x4 = torch.tensor([[1, 1, 1, 2]])
+x5 = torch.tensor([[1, 1, 2, 2]])
+x6 = torch.tensor([[1, 2, 1, 1]])
+kernel_metric3 = math.sqrt(kernel(x1, x1) - 2 * kernel(x1, x3) + kernel(x3, x3))
+kernel_metric4 = math.sqrt(kernel(x1, x1) - 2 * kernel(x1, x4) + kernel(x4, x4))
+kernel_metric5 = math.sqrt(
+    kernel(x1, x1) - 2 * kernel(x1, x5) + kernel(x5, x5))
+kernel_metric6 = math.sqrt(
+    kernel(x1, x1) - 2 * kernel(x1, x6) + kernel(x6, x6))
+print(kernel_metric3)
+print(kernel_metric5)
+print(kernel_metric4)
+print(kernel_metric6)
+
+# print(kernel(x1, x2))
+# print(kernel(x1, x3))
+# print(kernel(x1, x4))
+
 gpr = gp.models.GPRegression(X, y, kernel, noise=noise)
+# print(kernel(X, y))
+# print(kernel(X, y).size())
+# print(X)
+# print(y)
+# X2 = X.to('cpu').detach().numpy().copy()
+# y2 = y.to('cpu').detach().numpy().copy()
+# print(np.linalg.norm(X2 - y2, 2))
+# print(np.linalg.norm(y2 - X2, 2))
 
 Xtest = torch.linspace(-0.5, 5.5, 500)
 with torch.no_grad():
@@ -46,9 +77,9 @@ plt.xlabel("step")
 plt.ylabel("loss")
 plt.savefig('./image/fig03.pdf')
 plt.close()
-print('variance = {}'.format(gpr.kernel.variance))
-print('lengthscale = {}'.format(gpr.kernel.lengthscale))
-print('noise = {}'.format(gpr.noise))
+# print('variance = {}'.format(gpr.kernel.variance))
+# print('lengthscale = {}'.format(gpr.kernel.lengthscale))
+# print('noise = {}'.format(gpr.noise))
 
 Xtest = torch.linspace(-0.5, 5.5, 500)
 with torch.no_grad():
---
+++ gaussian-process/test02.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def my_kernel(x, x_prime, a, Lambda):
+    Lambda_inv = np.linalg.inv(Lambda)
+    return 1 * np.exp(- a / 2 * (x - x_prime).T.dot(Lambda_inv).dot(x - x_prime))
+
+
+def my_kernel_metric(x, x_prime, a, Lambda):
+    return np.sqrt(my_kernel(x, x, a, Lambda) - 2 * my_kernel(x, x_prime, a, Lambda) + my_kernel(x_prime, x_prime, a, Lambda))
+
+a = 1
+N = 1
+eta = 0.1
+Lambda = np.diag([0.1, 0.2, 0.3, 0.4])
+x1 = np.array([1, 1, 1, 1])
+x2 = np.array([2, 1, 1, 1])
+x3 = np.array([1, 2, 1, 1])
+x4 = np.array([1, 1, 2, 1])
+
+
+v = np.array([np.sqrt(1 / 3), np.sqrt(1 / 3), np.sqrt(1 / 3), 0])
+x_q = np.sqrt(2 * np.log(2 * (a ** 2) / (2 * (a ** 2) - ((N * eta) ** 2)))) * np.sqrt(Lambda).dot(v) + x1
+print(x_q)
+kernel_metric = my_kernel_metric(x1, x_q, a, Lambda)
+print(kernel_metric)
+
+# print(x_q)
+
+# x_q_tensor = torch.from_numpy(np.array([x_q]))
+# print(x_q_tensor)
+# kernel_metric = math.sqrt(
+#     kernel(x1, x1) - 2 * kernel(x1, x_q_tensor) + kernel(x_q_tensor, x_q_tensor))
+# print(kernel_metric)

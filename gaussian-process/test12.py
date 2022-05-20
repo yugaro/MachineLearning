4a43d0a1eacbfef9eaf446cbd510845ebff3b9cf
yugaro <yugaro.yugaro@gmail.com>
2022-05-20 14:41:19 +0900

    Initial Commit

--- gaussian-process/test12.py
+++ gaussian-process/test12.py
@@ -14,7 +14,7 @@ def km(e):
     return np.sqrt(2 * (alpha ** 2) - 2 * kernel(e))
 
 def dh(e):
-    return (b ** 2 / Lambda) * (e * kernel(e)) / km(e) - 1
+    return (kernel(e) / km(e)) * (b ** 2 / Lambda) * e - 1
 
 
 def dev(e):
@@ -23,44 +23,48 @@ def dev(e):
 def ddh(e):
     return 1 - (1 + (kernel(e) / (km(e) ** 2))) * (b ** 2 / Lambda) * e
 
-
-e = 0.01
-ddh_list = []
-for i in range(100000):
-    ddhv = ddh(e)
-    ddh_list.append(ddhv)
-    e += 0.01
-
-fig, ax = plt.subplots()
-ax.plot(ddh_list)
-plt.show()
-
 # e = 10
 # e_list = [e]
 # for i in range(1000):
-#     e_next = h(e)
+#     e_next = km(e)
 #     e_list.append(e_next)
 #     e = e_next
 # fig, ax = plt.subplots()
 # ax.plot(e_list)
 # plt.show()
 
-# e = 0.01
-# ev_list = []
-# for i in range(10000):
-#     ev = dh(e)
-#     ev_list.append(ev)
-#     e += 0.01
-# fig, ax = plt.subplots()
-# ax.plot(ev_list)
-# plt.show()
+e = 0.001
+e_list = []
+ev_list = []
+for i in range(100000):
+    e_list.append(e)
+    ev = dh(e)
+    ev_list.append(ev)
+    e += 0.001
+fig, ax = plt.subplots()
+ax.plot(e_list, ev_list)
+plt.show()
 
-# e = 0.01
+# e = 0.001
+# e_list = []
 # ddh_list = []
 # for i in range(1000):
+#     e_list.append(e)
 #     ddhv = dev(e)
 #     ddh_list.append(ddhv)
-#     e += 0.01
+#     e += 0.001
 # fig, ax = plt.subplots()
-# ax.plot(ddh_list)
+# ax.plot(e_list, ddh_list)
+# plt.show()
+
+# e = 0.001
+# e_list = []
+# ddh_list = []
+# for i in range(1000):
+#     e_list.append(e)
+#     ddhv = ddh(e)
+#     ddh_list.append(ddhv)
+#     e += 0.001
+# fig, ax = plt.subplots()
+# ax.plot(e_list, ddh_list)
 # plt.show()
--- gaussian-process/test13.py
+++ gaussian-process/test13.py
@@ -2,15 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def exf(x):
-    return 1 - (1 / 2) * ((x ** 2 * np.exp(- x ** 2 / 2)) / (1 - np.exp(- x ** 2 / 2)))
+    return 1 - (1 + (np.exp(- x ** 2 / 2) / 2 * (1 - np.exp(- x ** 2 / 2)))) * (x ** 2)
 
 exf_list = []
-x = 0.0001
+x = 0.001
+x_list = []
 for i in range(100000):
+    x_list.append(x)
     exfv = exf(x)
     exf_list.append(exfv)
     x += 0.0001
+    if exfv > 0:
+        print(x, exfv)
 
 fig, ax = plt.subplots()
-ax.plot(exf_list)
+ax.plot(x_list, exf_list)
 plt.show()
